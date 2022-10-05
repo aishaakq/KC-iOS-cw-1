51b9654067fd789406bd7b3a4a01b0ec9d203c16
عائشة القبندي - 389 <aqabandi23@gmail.com>
2022-10-05 19:09:02 +0300

    done

---
+++ DayOne/DayOne/DayOneApp.swift
@@ -0,0 +1,17 @@
+//
+//  DayOneApp.swift
+//  DayOne
+//
+//  Created by aisha abdulaziz on 05/10/2022.
+//
+
+import SwiftUI
+
+@main
+struct DayOneApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
